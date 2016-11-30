ac8ae0d88d0da4cd4bc403f063c364650697ba53
Nick Miller <nam99@pitt.edu>
2016-11-30 09:52:22 -0500

    Add files via upload

--- Assignment-10/solution.m
+++ Assignment-10/solution.m
@@ -14,9 +14,40 @@ for i = 1:20
         im = imresize(im, [227, 227]);
         im = im - image_mean;
         net.forward({im});
-        features{1,count} = net.blobs('fc6').get_data();
+        features{1,count} = net.blobs('fc8').get_data();
         features{2,count} = net.blobs('fc7').get_data();
-        features{3,count} = net.blobs('fc8').get_data();
+        features{3,count} = net.blobs('fc6').get_data();
+        features{4,count} = imgSetVector(i).Description;
+        features{5,count} = im;
         count = count + 1;
     end
 end
+
+[~,len] = size(features);
+
+p = randperm(len + 1);
+test_percentage = .2*len;
+test = p(1:test_percentage)-1;
+k = [1:1:len];
+
+
+Acommon = intersect(test,k);
+train = setxor(k,Acommon);
+
+train_set = {};
+
+for i = 1:length(train)
+    train_set{1,i} = features{1,train(i)};
+    train_set{2,i} = features{2,train(i)};
+    train_set{3,i} = features{3,train(i)};
+    train_set{4,i} = features{4,train(i)};
+end
+templateSVM('Standardize',1,'KernelFunction','linear');
+
+fc8_feature = cell2mat(train_set(1,:));
+fc7_feature = cell2mat(train_set(2,:));
+fc6_feature = cell2mat(train_set(3,:));
+
+fc8_svm = fitcecoc(fc8_feature',train_set(4,:)');
+fc7_svm = fitcecoc(fc7_feature',train_set(4,:)');
+fc6_svm = fitcecoc(fc6_feature',train_set(4,:)');
